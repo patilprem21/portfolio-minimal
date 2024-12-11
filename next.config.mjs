b99ad9eddb92101866114fc6e782ac58bfa4e4d7
patilprem21 <pprem2802@gmail.com>
2024-12-11 16:33:59 +0530

    Update Next.js config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/portfolio-minimal',
+    assetPrefix: '/portfolio-minimal',
+    // Ensure your images and other assets work
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
